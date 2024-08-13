c356a37c42a25b4998c8070d9d1e6fcce27838ef
MichalApl <123867268+MichalApl@users.noreply.github.com>
2024-08-13 14:16:59 +0300

    Create nav.php

---
+++ includes/nav.php
@@ -0,0 +1,8 @@
+<nav>
+        <div class="brand">CTFit</div>
+        <div class="links">
+            <a data-active="index" href="index.php">Shop</a>
+            <a data-active="about" href="about.php">About</a>
+            <a data-active="coupons" href="coupons.php">Coupons</a>
+        </div>
+    </nav>
