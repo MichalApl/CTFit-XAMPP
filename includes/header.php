44099fca8d2deb240203a75032bf696eb70c8941
MichalApl <123867268+MichalApl@users.noreply.github.com>
2024-08-13 14:17:27 +0300

    Create header.php

---
+++ includes/header.php
@@ -0,0 +1,6 @@
+<header>
+    <h1>Welcome to CTFit</h1>
+    <p class="sub-title">
+    We offer the highest quality fitness products on the market, and our website is exceptionally secure 
+    </p>
+</header>
